fa23be6edab3d8947d6d99d0be88bfb765c4273b
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-19 21:52:09 +0800

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous routine that spawns wait_random n times 
+    Asynchronous routine that spawns wait_random n times
     with the specified max_delay.
 
     Args:
