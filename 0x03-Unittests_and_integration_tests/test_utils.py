d1bc9d7f3d96d4c21f327ce583fef77b10a30fef
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-02-02 15:07:21 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,49 +2,71 @@
 """Unit tests for utils module."""
 
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+
+from typing import Dict, Mapping, Sequence, Union
 from utils import access_nested_map, get_json, memoize
 
-
 class TestAccessNestedMap(unittest.TestCase):
+    '''Test case for `utils.access_nested_map` function.
+    '''
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            expected: Union[Dict, int]
+            ) -> None:
+        '''Tests `access_nexted_map`'s output
+        '''
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
-        with self.assertRaises(expected_exception) as context:
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            exception: Exception,
+            ) -> None:
+        '''Tests `access_nested_map`'s exception raising.
+        '''
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
 
-        self.assertEqual(str(context.exception), f"'{path[-1]}' not in {str(nested_map)}",
-                f"Expected KeyError: '{path[-1]}' not in {str(nested_map)}")
-
-
 class TestGetJson(unittest.TestCase):
+    '''Tests the `utils.get_json` function.
+    '''
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
-        result = get_json(test_url)
-
-        mock_get.assert_called_once_with(test_url)
-        self.assertEqual(result, test_payload)
-
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        '''Tests `get_json`'s output.
+        '''
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
 
 class TestMemoize(unittest.TestCase):
-
-    @patch('utils.TestClass.a_method')
-    def test_memoize(self, mock_a_method):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
         class TestClass:
-
             def a_method(self):
                 return 42
 
@@ -52,15 +74,15 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        test_instance = TestClass()
-
-        result_1 = test_instance.a_property
-        result_2 = test_instance.a_property
-
-        self.assertEqual(result_1, 42)
-        self.assertEqual(result_2, 42)
-        mock_a_method.assert_called_once()
-
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
